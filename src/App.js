096ddd778930101f9b54dc65d272506c4644e82f
dcuddhy <dcuddhy@gmail.com>
2020-10-03 16:55:42 -0600

    get data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// This had ought to be in an .env
+const URL = 'http://localhost:3001/workout-data.json';
+
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(json => setFetchedData(json))
+    .catch(error => {
+      console.error('Something has gone wrong with your fetch operation: ', error);
+    });
+  }, [])
+
+  console.log('fetchedData : ', fetchedData);
+
   return (
     <div className="App">
       <header className="App-header">
